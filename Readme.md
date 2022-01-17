9ff98c5de6127400457b0a58b03b5d5355da447a
kelicaleb <kelicaleb@gmail.com>
2022-01-17 17:12:16 +0300

    ip4

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Bluehorizon Pizza 
+#### It is a website for odering pizza online, 17/01/2022}
+#### By **Caleb Kavindu Keli**
+## Description
+This is a website where one can oder their pizza online . The user chooses the size toppins, crust, toppins  amount, location if delivery is to be done  in which when submitted it returns the price.
+## Setup/Installation Requirements
+* Internet connection
+* Distinct description of your location
+## Known Bugs
+Currently the website has no known bugs after testing . 
+## Technologies Used
+The website was brought to life by the help of HTML for the webpage, Css for styling the webpage and Js for functinality
+## Support and contact details
+You can supprt me by reporting a bug or opinions pf hpw I can improve the wesite by contacting me through my email @kelicaleb7@gmail.com
+### License
+*Moringa school
+Copyright (c) {year} **Moringa school**
---
+++ main.js
@@ -0,0 +1,118 @@
+const form = document.querySelector("#content");
+form.addEventListener("submit", function(button){
+    button.preventDefault();
+    const jsform = new FormData(form);
+    const small = 450;
+    const medium = 750;
+    const large = 1000;
+    var calculate = jsform.get("number");
+    var location = jsform.get("local");
+    console.log(location);
+    if (jsform.get("size") == 1 && jsform.get("number") > 0 && jsform.get("local")){
+        const delivery = small * calculate + 500;
+        Swal.fire({
+            title: 'Your total amount plus delivery is ' + delivery,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', 'Delivery will take some few minutes!', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+    }
+    else if(jsform.get("size") == 1 && jsform.get("number") > 0){
+        const price = small * calculate;
+        Swal.fire({
+            title: 'Your total amount plus delivery ' + price,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+    }        
+    else if(jsform.get("size") == 2 && jsform.get("number") > 0 && jsform.get("local")){
+        const delivery = medium * calculate + 500;
+        Swal.fire({
+            title: 'Your total amount plus delivery ' + delivery,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', 'Delivery will take some few minutes!', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+
+    }
+    else if(jsform.get("size") == 2 && jsform.get("number") > 0){
+        const price = medium * calculate;
+        Swal.fire({
+            title: 'Your total amount is ' + price,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+    }        
+    else if(jsform.get("size") == 3 && jsform.get("number") > 0 && jsform.get("local")){
+        const delivery = large * calculate + 500;
+        Swal.fire({
+            title: 'Your total amount plus delivery is  ' + delivery,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', 'Delivery will take some few minutes!', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+    }
+    else if(jsform.get("size") == 3 && jsform.get("number") > 0){
+        const price = large * calculate;
+        Swal.fire({
+            title: 'Your total amount is ' + price,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+    }        
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Ensure all fields are filled !',
+          })
+    }
+
+
+})
+
